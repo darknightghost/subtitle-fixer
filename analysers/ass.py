e72bb163a09f5c6053ae12a3cb7539de7d32020d
darknightghost <darknightghost.cn@gmail.com>
2016-07-20 00:08:22 +0800

    UTC:2016-07-19 16:08:22 :

--- analysers/ass.py
+++ analysers/ass.py
@@ -32,7 +32,7 @@ class Line:
         self.data = data
 
     def __str__(self):
-        return data
+        return self.data + "\n"
 
 class FormatLine(Line):
     def __init__(self, data):
@@ -44,22 +44,117 @@ class FormatLine(Line):
             elif w.strip() == "End":
                 self.end_index = n
             n = n + 1
-        print(self.start_index)
-        print(self.end_index)
 
     def __str__(self):
-        return data
+        return self.data + "\n"
 
 class DialogueLine(Line):
     def __init__(self, data, format_line):
-        self.data = data
+        self.start_index = format_line.start_index
+        self.end_index = format_line.end_index
+
+        n = len("Dialogue:")
+        i = 0
+
+        while data[n].isspace():
+            n = n + 1
+
+        if self.start_index < self.end_index:
+            #Get str1
+            while i < self.start_index:
+                if data[n] == ',':
+                    i = i + 1
+                n = n + 1
+            self.str1 = data[: n]
+            data = data[n :]
+            
+            #Get start time string
+            n = 0
+            while data[n] != ",":
+                n = n + 1
+            time_str = data[: n].strip()
+            data = data[n :]
+            n = 0
+
+            #Get start time
+            self.start = time_to_sec(time_str.split(".")[0])
+            self.p_start = int(time_str.split(".")[1])
+
+            #Get str2
+            while i < self.end_index:
+                if data[n] == ',':
+                    i = i + 1
+                n = n + 1
+            self.str2 = data[: n]
+            data = data[n :]
+
+            #Get end time string
+            n = 0
+            while data[n] != ",":
+                n = n + 1
+            time_str = data[: n].strip()
+            data = data[n :]
+            self.str3 = data
+
+            #Get end time
+            self.end = time_to_sec(time_str.split(".")[0])
+            self.p_end = int(time_str.split(".")[1])
+
+        else:
+            #Get str1
+            while i < self.end_index:
+                if data[n] == ',':
+                    i = i + 1
+                n = n + 1
+            self.str1 = data[: n]
+            data = data[n :]
+            
+            #Get end time string
+            n = 0
+            while data[n] != ",":
+                n = n + 1
+            time_str = data[: n].strip()
+            data = data[n :]
+            n = 0
+
+            #Get end time
+            self.end = time_to_sec(time_str.split(".")[0])
+            self.p_end = int(time_str.split(".")[1])
+
+            #Get str2
+            while i < self.start_index:
+                if data[n] == ',':
+                    i = i + 1
+                n = n + 1
+            self.str2 = data[: n]
+            data = data[n :]
+
+            #Get start time string
+            n = 0
+            while data[n] != ",":
+                n = n + 1
+            time_str = data[: n].strip()
+            data = data[n :]
+            self.str3 = data
+
+            #Get start time
+            self.start = time_to_sec(time_str.split(".")[0])
+            self.p_start = int(time_str.split(".")[1])
+
 
     def __str__(self):
-        return data
-
+        if self.start_index < self.end_index:
+            return self.str1 + sec_to_time(self.start) + ".%.2d"%(self.p_start) \
+                    + self.str2 + sec_to_time(self.end) + ".%.2d"%(self.p_end) \
+                    + self.str3 + "\n"
+        else:
+            return self.str1 + sec_to_time(self.end) + ".%.2d"%(self.p_end) \
+                    + self.str2 + sec_to_time(self.start) + ".%.2d"%(self.p_start) \
+                    + self.str3 + "\n"
 
 class analyser(base_analyser):
     def __init__(self, path, encoding):
+        self.encoding = encoding
         f = open(path, "r",  encoding = encoding)
         lines = f.readlines()
         f.close()
@@ -85,10 +180,21 @@ class analyser(base_analyser):
         return
 
     def speed(self, rate):
-        pass
+        for l in self.lines:
+            if type(l) == DialogueLine:
+                l.start = l.start * rate
+                l.end = l.end * rate
+        return
 
     def offset(self, offset):
-        pass
+        for l in self.lines:
+            if type(l) == DialogueLine:
+                l.start = l.start + offset
+                l.end = l.end + offset
+        return
 
     def save(self, path):
-        pass
+        f = open(path, "w", encoding = self.encoding)
+        for l in self.lines:
+            f.write(str(l))
+        f.close()
